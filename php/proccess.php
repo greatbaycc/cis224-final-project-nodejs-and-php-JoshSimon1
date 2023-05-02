144dda442411185b0abea4bfcfa93f4d78daafa9
JoshSimon1 <125414208+JoshSimon1@users.noreply.github.com>
2023-05-02 19:33:05 -0400

    Create proccess.php

---
+++ php/proccess.php
@@ -0,0 +1,14 @@
+<?php
+if($_POST['submit'] == "Submit") 
+{
+   $name = $_POST['fullName'];
+   $email = $_POST['email'];
+   $about = $_POST['about'];
+   $picutre = $_POST['picture'];
+
+   
+
+   
+}
+
+?> 
